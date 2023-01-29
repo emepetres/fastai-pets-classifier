3bb5483d890dce8e823e798da40a885b245832f2
Javi Carnero <javier@carnero.net>
2023-01-29 22:21:30 +0100

    learner refactor

--- README.md
+++ README.md
@@ -1,19 +1,19 @@
 # fastai-pets-classifier
 
-Pets classifier using fastai
+Pets classifier using fastai. Also a second sample using raw pytorch for data, model and optimizer, except the learner.
 
-## NOTES:
+## NOTES
 
-* stage_1 & stage_2 is equivalent of learn.fine_tune(4, freeze_epochs=4)
+* stage_1 & stage_2 is equivalent of learn.fine_tune(4, freeze_epochs=4). lr and other hyps can be tweaked as well
 * lr_max=slice(..) actually allows you to use different lr for body and head
 * learner creation should be refactored to its own class, so you can create and load weights to it in stage 2
-
 
 ## Run it
 
 ```bash
 conda create -f environment.yml
 conda activate fastai
+cd src
 python -W ignore train_stage_1.py
 python -W ignore train_stage_2.py
 ```
--- src/train_stage_1.py
+++ src/train_stage_1.py
@@ -1,45 +1,7 @@
-# This import * is an extremely bad practice in modern Python programming.
-# This shouldn’t be done in production
-from fastai.vision.all import *
-
-import config
+from learner import get_learner
 
 if __name__ == "__main__":
-    set_seed(config.SEED)
-
-    path = untar_data(URLs.PETS)
-    fnames = get_image_files(path / "images")
-    pat = r"(.+)_\d+.jpg$"
-
-    item_tfms = RandomResizedCrop(460, min_scale=0.75, ratio=(1.0, 1.0))
-    batch_tfms = [
-        *aug_transforms(size=224, max_warp=0),
-        Normalize.from_stats(*imagenet_stats),
-    ]
-    bs = 64
-
-    # # dls = ImageDataLoaders.from_name_re(
-    # #     path,  # The location of the data
-    # #     fnames,  # A list of filenames
-    # #     pat,  # A regex pattern to extract the labels
-    # #     item_tfms=item_tfms,  # Transform augmentations to be applied per item
-    # #     batch_tfms=batch_tfms,  # Transform augmentations to be applied per batch
-    # #     bs=bs,  # How many examples should be drawn each time
-    # # )
-
-    pets = DataBlock(
-        blocks=(ImageBlock, CategoryBlock),
-        get_items=get_image_files,
-        splitter=RandomSplitter(),
-        get_y=RegexLabeller(pat=r"/([^/]+)_\d+.*"),
-        item_tfms=item_tfms,
-        batch_tfms=batch_tfms,
-    )
-
-    path_im = path / "images"
-    dls = pets.dataloaders(path_im, bs=bs)
-
-    learn = vision_learner(dls, resnet34, metrics=error_rate)
+    learn = get_learner()
     learn.fit_one_cycle(4)
 
-    learn.save('stage_1')
+    learn.save("stage_1")
