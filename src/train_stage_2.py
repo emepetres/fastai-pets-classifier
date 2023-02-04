2ed87dfd0ac670f4fd0ad6d04b69d6e57771a9e9
Javi Carnero <javier@carnero.net>
2023-02-04 12:09:40 +0100

    fix unfreeze in stage 2

--- src/train_stage_2.py
+++ src/train_stage_2.py
@@ -5,6 +5,7 @@ if __name__ == "__main__":
     learn = get_learner()
     learn.load("stage_1")
 
+    learn.unfreeze()
     learn.fit_one_cycle(4, lr_max=slice(1e-6, 1e-4))
 
     learn.save("stage_2")
